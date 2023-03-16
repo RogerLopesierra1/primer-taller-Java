87db4958216246574cb3c37a587002fd0e374b7d
Roger Lopesierra <rogerlopesierraolivera@gmail.com>
2023-03-15 21:27:07 -0500

    creacion y terminacion de la clase Cuenta

---
+++ src/cuenta_bancaria/Cuenta.java
@@ -0,0 +1,52 @@
+package cuenta_bancaria;
+
+public class Cuenta {
+	protected float saldo;
+    protected int numConsignaciones = 0;
+    protected int numRetiros = 0;
+    protected float tasaAnual;
+    protected float comisionMensual = 0;
+    
+	public Cuenta(float saldo, float tasaAnual) {
+		super();
+		this.saldo = saldo;
+		this.tasaAnual = tasaAnual;
+	}
+	
+	public void consignar(int cantidad) {
+		saldo += cantidad;
+        numConsignaciones++;
+	}
+	
+	public void retirar(int cantidad) {
+		if (cantidad <= saldo) {
+            saldo -= cantidad;
+            numRetiros++;
+        } else {
+            System.out.println("Error: Maximo puede retirar "+this.saldo);
+        }
+	}
+	
+	public void calcularInteresMensual() {
+		float interes = saldo * (tasaAnual / 12);
+        saldo += interes;
+	}
+    
+	public void extractoMensual() {
+		saldo -= comisionMensual;
+		calcularInteresMensual();
+		numConsignaciones = 0;
+        numRetiros = 0;
+        comisionMensual = 0;
+	}
+	
+	public void imprimir() {
+        System.out.println("                   Saldo: " + saldo);
+        System.out.println("Número de consignaciones: " + numConsignaciones);
+        System.out.println("       Número de retiros: " + numRetiros);
+        System.out.println("              Tasa anual: " + tasaAnual);
+        System.out.println("        Comisión mensual: " + comisionMensual);
+    }
+   
+	
+}
