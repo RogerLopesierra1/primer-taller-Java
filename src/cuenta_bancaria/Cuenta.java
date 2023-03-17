6c1b536ebbaa07de86531b72505d8683590aeb30
Roger Lopesierra <rogerlopesierraolivera@gmail.com>
2023-03-16 20:35:30 -0500

    documentacion calde Cuenta

--- src/cuenta_bancaria/Cuenta.java
+++ src/cuenta_bancaria/Cuenta.java
@@ -1,5 +1,10 @@
 package cuenta_bancaria;
 
+/**
+ * Esta clase representa una cuenta bancaria con un saldo, tasa de interés anual,
+ * y funciones para depositar, retirar, calcular intereses, generar un extracto 
+ * mensual e imprimir la información de la cuenta.
+ */
 public class Cuenta {
 	protected float saldo;
     protected int numConsignaciones = 0;
@@ -7,17 +12,36 @@ public class Cuenta {
     protected float tasaAnual;
     protected float comisionMensual = 0;
     
+    /**
+     * Crea una nueva cuenta bancaria con el saldo inicial 
+     * y tasa de interés anual especificados.
+     * 
+     * @param saldo: es el saldo inicial de la cuenta.
+     * @param tasaAnual: es la tasa de interés anual de la cuenta.
+     */
 	public Cuenta(float saldo, float tasaAnual) {
 		super();
 		this.saldo = saldo;
 		this.tasaAnual = tasaAnual;
 	}
 	
+	/**
+     * Deposita la cantidad especificada en la cuenta.
+     * 
+     * @param cantidad: es la cantidad a depositar.
+     */
 	public void consignar(float cantidad) {
 		saldo += cantidad;
         numConsignaciones++;
 	}
 	
+	/**
+     * Retira la cantidad especificada de la cuenta, si es posible.
+     * Si la cantidad a retirar es mayor que el saldo disponible, 
+     * no se realiza la operación y se muestra un mensaje de error.
+     * 
+     * @param cantidad: es la cantidad a retirar.
+     */
 	public void retirar(float cantidad) {
 		if (cantidad <= saldo) {
             saldo -= cantidad;
@@ -27,11 +51,21 @@ public class Cuenta {
         }
 	}
 	
+	/**
+     * Calcula los intereses mensuales de la cuenta y los agrega al saldo.
+     */
 	public void calcularInteresMensual() {
 		float interes = saldo * tasaAnual / (12*100);
         saldo += interes;
 	}
     
+	/**
+     * Genera el extracto mensual de la cuenta, incluyendo la comisión mensual, los intereses generados y el número de consignaciones 
+     * y retiros realizados en el mes.
+     * 
+     * Reinicia el contador de consignaciones y retiros, 
+     * y establece la comisión mensual a cero.
+     */
 	public void extractoMensual() {
 		saldo -= comisionMensual;
 		calcularInteresMensual();
@@ -40,6 +74,11 @@ public class Cuenta {
         comisionMensual = 0;
 	}
 	
+	/**
+     * Imprime la información de la cuenta, incluyendo el saldo actual,
+     * el número de consignaciones y retiros realizados,
+     * la tasa de interés anual y la comisión mensual.
+     */
 	public void imprimir() {
         System.out.println("                   Saldo: " + saldo);
         System.out.println("Número de consignaciones: " + numConsignaciones);
